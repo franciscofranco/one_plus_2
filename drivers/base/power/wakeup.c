aa067aa511b4b4dbb7ed9bbc7aece66cea049ad0
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-24 02:54:18 +0000

    drivers: wakeup: don't print wakeup sources and block wakeups during screen on
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -17,6 +17,7 @@
 #include <linux/types.h>
 #include <trace/events/power.h>
 #include <linux/moduleparam.h>
+#include <linux/display_state.h>
 
 #include "power.h"
 
@@ -463,7 +464,7 @@ static bool wakeup_source_blocker(struct wakeup_source *ws)
 {
 	unsigned int wslen = 0;
 
-	if (ws && ws->active) {
+	if (!is_display_on() && ws && ws->active) {
 		wslen = strlen(ws->name);
 
 		if ((!enable_ipa_ws && !strncmp(ws->name, "IPA_WS", wslen)) ||
@@ -797,6 +798,10 @@ void print_active_wakeup_sources(void)
 	struct wakeup_source *ws;
 	int active = 0;
 	struct wakeup_source *last_activity_ws = NULL;
+
+	// kinda pointless to force this routine during screen on
+	if (is_display_on())
+		return;
 
 	rcu_read_lock();
 	list_for_each_entry_rcu(ws, &wakeup_sources, entry) {
